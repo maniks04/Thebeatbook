597e297096dbe55e0209081b78518809d133055b
Steven Riley <rileystevend@gmail.com>
2018-03-22 17:35:31 -0500

    artists can now search for venues and it appears in table

--- client/src/components/searchVenues.jsx
+++ client/src/components/searchVenues.jsx
@@ -4,16 +4,35 @@ import * as actions from '../actions/index.js';
 import { bindActionCreators } from 'redux';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import { Input } from 'antd';
+import { Input, Table } from 'antd';
 const Search = Input.Search;
 import axios from 'axios';
+
+
+const columns = [{
+  title: 'Name',
+  dataIndex: 'venue_name',
+  sorter: true,
+  width: '40%',
+}, {
+  title: 'Capacity',
+  dataIndex: 'capacity',
+  sorter: true,
+  width: '20%',
+}, {
+  title: 'Address',
+  dataIndex: 'venue_address',
+}];
 
 class SearchVenues extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      venues: []
+      venues: [/*{venue_id: 2, user_id: 3, venue_name: "Empire Control Room", venue_description: null, capacity: 800, venue_address: "700 Red River Dr."}*/],
+      pagination: {},
+      loading: false,
+      fetched: false
     }
   }
 
@@ -31,24 +50,82 @@ class SearchVenues extends React.Component {
     }).then(res => {
       console.log(res.data)
       this.setState({
-        venues: res.data
+        fetched: true,
+        venues: res.data.venues
       })
     }).catch(err => {
       console.log('error', err)
     })
   }
 
+  handleTableChange (pagination, filters, sorter) {
+    // const pager = { ...this.state.pagination };
+    // pager.current = pagination.current;
+    // this.setState({
+    //   pagination: pager,
+    // });
+    // this.fetch({
+    //   results: pagination.pageSize,
+    //   page: pagination.current,
+    //   sortField: sorter.field,
+    //   sortOrder: sorter.order,
+    //   ...filters,
+    // });
+  }
+  fetch (params = {}) {
+    // console.log('params:', params);
+    // this.setState({ loading: true });
+    // axios({
+    //   url: '/venues',
+    //   method: 'get',
+    //   data: {
+    //     results: 10,
+    //     ...params,
+    //   },
 
+    // }).then((data) => {
+    //   const pagination = { ...this.state.pagination };
+    //   // Read total count from server
+    //   // pagination.total = data.totalCount;
+    //   pagination.total = 200;
+    //   this.setState({
+    //     loading: false,
+    //     venues: data.results,
+    //     pagination,
+    //   });
+    // });
+  }
 
   render() {
-    return (
-      <Search
-      placeholder="Enter A City Here"
-      onSearch={value => this.onSearch(value)}
-      style={{ width: 300 }}
-      enterButton
+    if (this.state.fetched === false) {
+      return (
+        <Search
+        placeholder="Enter A City Here"
+        onSearch={value => this.onSearch(value)}
+        style={{ width: 300 }}
+        enterButton
+        />
+      );
+    } else {
+      return (
+        <div>
+        <Search
+        placeholder="Enter A City Here"
+        onSearch={value => this.onSearch(value)}
+        style={{ width: 300 }}
+        enterButton
+        />
+        <Table 
+        columns={columns}
+        rowKey={(record) => record.registered}
+        dataSource={this.state.venues}
+        pagination={this.state.pagination}
+        loading={this.state.loading}
+        onChange={this.handleTableChange}
       />
-    )
+      </div>
+      ); 
+    }
   }
 }
 
