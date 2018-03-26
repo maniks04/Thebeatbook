2f3e3a5ba59dfdfb04c6ff87a1379d6bb0878258
Steven Riley <rileystevend@gmail.com>
2018-03-26 17:48:05 -0500

    added airbnb linter

--- database/index.js
+++ database/index.js
@@ -1,18 +1,12 @@
 const config = require('./config.js');
 
-knex = require('knex')({
+const knex = require('knex')({
   client: 'mysql',
   connection: config,
 });
 
-
-
-const checkCredentials = (username) => {
-  return knex.select().from('users')
-    .where(knex.raw(`LOWER(username) = LOWER('${username}')`));
-};
-
-
+const checkCredentials = username => knex.select().from('users')
+  .where(knex.raw(`LOWER(username) = LOWER('${username}')`));
 
 const addUsers = async (username, password, email, userType) => {
   const userQuery = await knex.select().from('users')
@@ -23,132 +17,97 @@ const addUsers = async (username, password, email, userType) => {
     return 'username already exists';
   } else if (emailQuery.length) {
     return 'email already exists';
-  } else {
-     return await knex('users').insert({ username: username, password: password, email: email, user_type: userType});
   }
+  return knex('users').insert({
+    username, password, email, user_type: userType,
+  });
 };
-
-
-const registerArtist = async (username, password, email, city, state) => {
-  let result = await addUsers(username, password, email, 'artist')
-  if (result === 'username already exists' || result === 'email already exists') {
-    return result;
-  } else {
-      const id = await getUserID(username)
-      return await knex('artists').insert({artist_name: username, artist_city: city, artist_state: state, user_id: id})
-  }
-}
-
-const registerVenue = async (username, password, email, venueName, address, city, state, capacity) => {
-  let result = await addUsers(username, password, email, 'venue')
-  if (result === 'username already exists' || result === 'email already exists') {
-    return result;
-  } else {
-      const id = await getUserID(username)
-      return await knex('venues').insert({venue_name: venueName, venue_address: address, venue_city: city, venue_state: state, capacity: capacity, user_id: id})//id, name, capacity, city, state, address
-  }
-}
 
 const getUserID = async (username) => {
-  let id = await knex.select('user_id').from('users').where('username', username);
-  return id[0].user_id
-}
-
-const getUserByName = async (username) => {
-  let user = await knex.select('*').from('users').where('username', username);
-  return user[0];
-}
-
-
-//REFACTOR TO OPTIMIZE DB QUERY: done for artists, not venues yet
-const getUser = async (username) => {
-  let user = await knex.select('*').from('users').where('username', username);
-  if (user[0].user_type === 'artist') {
-    let artist = await getArtist(user[0].user_id);
-    let bookings = await getArtistBookings2(artist.artist_id);
-    return [user[0], artist, bookings]
-  } else {
-    let venue = await getVenue(user[0].user_id)
-    let bookings = await getVenueBookings2(venue.venue_id);
-    return [user[0], venue, bookings]
-  }
+  const id = await knex.select('user_id').from('users').where('username', username);
+  return id[0].user_id;
 };
 
+const registerArtist = async (username, password, email, city, state) => {
+  const result = await addUsers(username, password, email, 'artist');
+  if (result === 'username already exists' || result === 'email already exists') {
+    return result;
+  }
+  const id = await getUserID(username);
+  return knex('artists').insert({
+    artist_name: username, artist_city: city, artist_state: state, user_id: id,
+  });
+};
 
+const registerVenue = async (username, password, email, venueName, address, city, state, capacity) => {
+  const result = await addUsers(username, password, email, 'venue');
+  if (result === 'username already exists' || result === 'email already exists') {
+    return result;
+  }
+  const id = await getUserID(username);
+  return knex('venues').insert({
+    venue_name: venueName, venue_address: address, venue_city: city, venue_state: state, capacity, user_id: id,
+  });
+};
 
+const getUser = async (username) => {
+  const user = await knex.select('*').from('users').where('username', username);
+  if (user[0].user_type === 'artist') {
+    const artist = await getArtist(user[0].user_id);
+    const bookings = await getArtistBookings2(artist.artist_id);
+    return [user[0], artist, bookings];
+  }
+  const venue = await getVenue(user[0].user_id);
+  const bookings = await getVenueBookings2(venue.venue_id);
+  return [user[0], venue, bookings];
+};
 
 const getArtist = async (userId) => {
-  let artist = await knex.select('*').from('artists').where('artists.user_id', userId);
+  const artist = await knex.select('*').from('artists').where('artists.user_id', userId);
   return artist[0];
-}
+};
 
 const getVenue = async (userId) => {
-  let venue = await knex.select('*').from('venues').where('venues.user_id', userId);
+  const venue = await knex.select('*').from('venues').where('venues.user_id', userId);
   return venue[0];
-}
-
-const getVenues = async (city) => {
-  return await knex.select('*').from('venues').where('venues.venue_city', city);
-}
-
-//BOOKINGS
-
-const getArtistBookings = (artistId) => {
-  return knex.select('*')
-    .from('bookings')
-    .where('bookings.artist_id', artistId)
-    .orderBy('bookings.start_time', 'booking_description');
 };
 
-const getVenueBookings = (venueId) => {
-  return knex.select('*')
-    .from('bookings')
-    .where('bookings.venue_id', venueId)
-    .orderBy('bookings.start_time', 'booking_description');
-};
+const getVenues = city => knex.select('*').from('venues').where('venues.venue_city', city);
 
 const addBooking = async (info) => {
-  await knex('bookings').insert({ 
-    artist_id: info.artistId, 
-    venue_id: info.venueId, 
-    start_time: info.start_time, 
-    end_time: info.end_time, 
+  await knex('bookings').insert({
+    artist_id: info.artistId,
+    venue_id: info.venueId,
+    start_time: info.start_time,
+    end_time: info.end_time,
     booking_description: info.booking_description,
-    booking_title: info.booking_title
+    booking_title: info.booking_title,
   });
-}
+};
 
 const updateBooking = async (info) => {
-  let toggle = info.confirmed === 0 ? 1 : 0;
-  await knex('bookings').where('booking_id', info.booking_id).update({ 
-    confirmed: toggle
+  const toggle = info.confirmed === 0 ? 1 : 0;
+  await knex('bookings').where('booking_id', info.booking_id).update({
+    confirmed: toggle,
   });
-}
-
-const getVenueBookings2 = async (venueId) => {
-  return await knex.column(knex.raw('b.*, a.artist_name')).select()
-    .from(knex.raw('bookings b'))
-    .innerJoin(knex.raw('artists a using (artist_id)'))    
-    .where(knex.raw(`b.venue_id = ${venueId}`))
-    .orderBy('b.start_time', 'desc');
 };
-//SELECT b.*, v.venue_name
-//FROM bookings b INNER JOIN venues v using (venue_id)
-//WHERE v.venue_id = <your venue id>
-//ORDER BY b.start_time DESC
 
-const getArtistBookings2 = async (artistId) => {
-  return await knex.column(knex.raw('b.*, v.venue_name')).select()
-    .from(knex.raw('bookings b'))
-    .innerJoin(knex.raw('venues v using (venue_id)'))    
-    .where(knex.raw(`b.artist_id = ${artistId}`))
-    .orderBy('b.start_time', 'desc');
-};
+const getVenueBookings2 = async venueId => knex.column(knex.raw('b.*, a.artist_name')).select()
+  .from(knex.raw('bookings b'))
+  .innerJoin(knex.raw('artists a using (artist_id)'))
+  .where(knex.raw(`b.venue_id = ${venueId}`))
+  .orderBy('b.start_time', 'desc');
+
+const getArtistBookings2 = async artistId => knex.column(knex.raw('b.*, v.venue_name')).select()
+  .from(knex.raw('bookings b'))
+  .innerJoin(knex.raw('venues v using (venue_id)'))
+  .where(knex.raw(`b.artist_id = ${artistId}`))
+  .orderBy('b.start_time', 'desc');
 
 const getEpk = async (artistId) => {
-  let artist = await knex.select('*').from('artists').where('artists.artist_id', artistId);
+  const artist = await knex.select('*').from('artists').where('artists.artist_id', artistId);
   return artist[0];
-}
+};
 
 module.exports = {
   registerArtist,
@@ -160,6 +119,6 @@ module.exports = {
   getVenues,
   getEpk,
   getVenueBookings2,
-  updateBooking
+  updateBooking,
 };
 
