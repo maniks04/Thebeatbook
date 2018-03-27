ad8ef25b92c5ac55991bea1d8903f10430f58ce2
Trent <trenthendricks2@gmail.com>
2018-03-27 15:02:45 -0500

    adds new input fields

--- client/src/components/epkEdit.jsx
+++ client/src/components/epkEdit.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import $ from 'jquery';
 import axios from 'axios';
-import { Form, Icon, Input, Button, Upload, message } from 'antd'; /* eslint-disable-line */
+import { Form, Icon, Input, Button, Upload, message, Col, Row } from 'antd'; /* eslint-disable-line */
 const FormItem = Form.Item;
 const { TextArea } = Input;
 
@@ -38,11 +38,11 @@ class NormalLoginForm extends React.Component {
 
   onClick() {
     const artist_id = this.props.artistID;
+    const { imageUrl } = this.state;
     let artist_name = $('.band').val();
     let artist_description = $('.description').val();
     let artist_city = $('.city').val();
     let artist_state = $('.state').val();
-    let { imageUrl } = this.state;
 
     if (artist_name === '') {
       artist_name = this.state.artist_name; /* eslint-disable-line */
@@ -95,54 +95,86 @@ class NormalLoginForm extends React.Component {
     const { imageUrl } = this.state;
 
     return (
-      <Form className="login-form">
-        <FormItem> Bane Name?
-          <Input
-            className="band"
-            placeholder={this.state.artist_name}
-          />
-        </FormItem>
-        <FormItem> Your City?
-          <Input
-            className="city"
-            placeholder={this.state.artist_city}
-          />
-        </FormItem>
-        <FormItem> Your State
-          <Input
-            className="state"
-            placeholder={this.state.artist_state}
-          />
-        </FormItem>
-        <FormItem> Band Bio
-          <TextArea
-            className="description"
-            placeholder={this.state.artist_description}
-            autosize={{ minRows: 4, maxRows: 12 }}
-          />
-        </FormItem>
-        <div> Change Image of your band.</div>
-        <Upload
-          name="avatar"
-          listType="picture-card"
-          className="avatar-uploader"
-          showUploadList={false}
-          action="http://localhost:3000/epkImgUpload"
-          onChange={val => this.handleChange(val)}
-        >
-          {imageUrl ? <img src={imageUrl} alt="" /> : uploadButton}
-        </Upload>
-        <FormItem>
-          <Button
-            type="primary"
-            htmlType="submit"
-            onClick={() => this.onClick()}
-            className="login-form-button"
-          >
-            Save Changes
-          </Button>
-        </FormItem>
-      </Form>
+      <Row gutter={40}>
+        <Col span={8}>
+          <FormItem> Bane Name
+            <Input
+              className="band"
+              placeholder={this.state.artist_name}
+            />
+          </FormItem>
+          <FormItem> Your City
+            <Input
+              className="city"
+              placeholder={this.state.artist_city}
+            />
+          </FormItem>
+          <FormItem> Your State
+            <Input
+              className="state"
+              placeholder={this.state.artist_state}
+            />
+          </FormItem>
+        </Col>
+        <Col span={8}>
+          <Form className="login-form">
+            <FormItem> Band Bio
+              <TextArea
+                className="description"
+                placeholder={this.state.artist_description}
+                autosize={{ minRows: 4, maxRows: 12 }}
+              />
+            </FormItem>
+            <div> Change Image of your band.</div>
+            <Upload
+              name="avatar"
+              listType="picture-card"
+              className="avatar-uploader"
+              showUploadList={false}
+              action="http://localhost:3000/epkImgUpload"
+              onChange={val => this.handleChange(val)}
+            >
+              {imageUrl ? <img src={imageUrl} alt="" /> : uploadButton}
+            </Upload>
+            <FormItem>
+              <Button
+                type="primary"
+                htmlType="submit"
+                onClick={() => this.onClick()}
+                className="login-form-button"
+              >
+                Save Changes
+              </Button>
+            </FormItem>
+          </Form>
+        </Col>
+        <Col span={8}>
+          <FormItem>
+            Facebook
+            <Input
+              prefix={<Icon type="facebook" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              className="facebook"
+              placeholder="Facebook.facebook"
+            />
+          </FormItem>
+          <FormItem>
+            Instagram
+            <Input
+              prefix={<Icon type="instagram" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              className="instagram"
+              placeholder="Instagram/insta"
+            />
+          </FormItem>
+          <FormItem>
+            Twitter
+            <Input
+              prefix={<Icon type="twitter" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              className="twitter"
+              placeholder="twitter/twit"
+            />
+          </FormItem>
+        </Col>
+      </Row>
     );
   }
 }
