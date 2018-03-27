36e3c0e45805b594599e70e42722fc2873691360
Steven Riley <rileystevend@gmail.com>
2018-03-27 12:00:36 -0500

    searchVenues sort working and rest of repo linted

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,31 +1,30 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Form} from 'antd';
+import { Form } from 'antd';
 import * as actions from '../actions/index.js';
-import {open} from '../actions'
 import LoginForm from './loginform.jsx';
-import logo from '../../../beatbooklogo.png'
+import logo from '../../../beatbooklogo.png';
 
-const LoginFormContainer = Form.create()(LoginForm); //component for antd loginform
+const LoginFormContainer = Form.create()(LoginForm); // component for antd loginform
 
 class Login extends React.Component {
   constructor(props) {
-      super(props)
+    super(props);
   }
 
   render() {
-    return(
+    return (
       <div style={styles.loginbox}>
-        <img src={logo} style={styles.logo}></img>
+        <img src={logo} style={styles.logo} alt="" />
         <div style={styles.beatbook}>beatbook</div>
-        <div style={styles.divider}></div>
+        <div style={styles.divider} />
         <div style={styles.loginform}>
-          <LoginFormContainer/>
+          <LoginFormContainer />
         </div>
         <div className="fb-login-button" data-size="medium" data-auto-logout-link="true">facebook login</div>
       </div>
-    )
+    );
   }
 }
 
@@ -39,48 +38,48 @@ const mapDispatchToProps = dispatch => (
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
 
-
 const styles = {
   logo: {
     height: 20,
     width: 20,
-    display: 'inline-block'
+    display: 'inline-block',
 
   },
   beatbook: {
     fontSize: 20,
     fontFamily: 'system-ui',
     marginTop: '5%',
-    display: 'inline-block'
+    display: 'inline-block',
   },
   loginbutton: {
-    textAlign: 'center'
+    textAlign: 'center',
   },
   loginbox: {
     backgroundColor: 'white',
     position: 'absolute',
     borderStyle: 'solid',
-    borderWidth: .5,
+    borderWidth: 0.5,
     borderColor: '#e6e6e6',
     width: '25%',
     height: '75%',
     left: '37.5%',
     top: '12.5%',
     textAlign: 'center',
-    overflow:'auto'
+    overflow: 'auto',
   },
   loginform: {
     marginLeft: 50,
-    marginRight: 50
+    marginRight: 50,
 
   },
   divider: {
     borderStyle: 'solid',
-    borderWidth: .5,
+    borderWidth: 0.5,
     borderColor: '#e6e6e6',
     marginLeft: 25,
     marginRight: 25,
     marginTop: 50,
-    marginBottom: 50
-  }
-}
+    marginBottom: 50,
+  },
+};
+
--- client/src/components/loginform.jsx
+++ client/src/components/loginform.jsx
@@ -10,35 +10,34 @@ const FormItem = Form.Item;
 
 class LoginForm extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
-
-  submitLogin (e)   {
+  submitLogin(e) {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        this.props.actions.submitLogin(values.userName, values.password)
+        this.props.actions.submitLogin(values.userName, values.password);
       }
     });
   }
 
   openRegisterModal() {
-    this.props.actions.openRegisterModal()  //sets register modal state to true
+    this.props.actions.openRegisterModal(); // sets register modal state to true
   }
 
   closeRegisterModal() {
-    this.props.actions.closeRegisterModal() //sets register modal state to false
+    this.props.actions.closeRegisterModal(); // sets register modal state to false
   }
 
   loadArtistRegisterPage() {
-    this.closeRegisterModal()
-    this.props.history.replace('/artistregister') //closes modal and loads artist registration form
+    this.closeRegisterModal();
+    this.props.history.replace('/artistregister'); // closes modal and loads artist registration form
   }
 
   loadVenueRegisterPage() {
-    this.closeRegisterModal()
-    this.props.history.replace('/venueregister') //closes modal and loads venue registration form
+    this.closeRegisterModal();
+    this.props.history.replace('/venueregister'); // closes modal and loads venue registration form
   }
 
   render() {
@@ -48,43 +47,40 @@ class LoginForm extends React.Component {
         <Modal
           visible={this.props.store.registerModalStatus}
           footer={[
-            <Button key={'venue'} onClick={() => this.loadVenueRegisterPage()}>Venue</Button>,
-            <Button key={'artist'} onClick={() => this.loadArtistRegisterPage()}>Artist</Button>
+            <Button key="venue" onClick={() => this.loadVenueRegisterPage()}>Venue</Button>,
+            <Button key="artist" onClick={() => this.loadArtistRegisterPage()}>Artist</Button>,
           ]}
           onCancel={() => this.closeRegisterModal()}
-        >
-      </Modal>
-      <Form onSubmit={(e) => this.submitLogin(e)} className="login-form">
-        <FormItem>
-          {getFieldDecorator('userName', {
+        />
+        <Form onSubmit={e => this.submitLogin(e)} className="login-form">
+          <FormItem>
+            {getFieldDecorator('userName', {
             rules: [{ required: true, message: 'Please input your username!' }],
           })(
-            <Input id="username" prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+            <Input id="username" prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />,
           )}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator('password', {
+          </FormItem>
+          <FormItem>
+            {getFieldDecorator('password', {
             rules: [{ required: true, message: 'Please input your Password!' }],
           })(
-            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />,
           )}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator('remember', {
+          </FormItem>
+          <FormItem>
+            {getFieldDecorator('remember', {
             valuePropName: 'checked',
             initialValue: true,
           })(
-            <Checkbox>Remember me</Checkbox>
+            <Checkbox>Remember me</Checkbox>,
           )}
-          <a className="login-form-forgot" href="">Forgot password</a>
-
-          <Button type="primary" htmlType="submit" className="login-form-button">
+            <a className="login-form-forgot" href="">Forgot password</a>
+            <Button type="primary" htmlType="submit" className="login-form-button">
             Log in
-          </Button>
-
+            </Button>
           Or <a onClick={() => this.openRegisterModal()}>register now!</a>
-        </FormItem>
-      </Form>
+          </FormItem>
+        </Form>
       </div>
     );
   }
