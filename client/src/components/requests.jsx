6247d4c5ea78d00d3f5c3b3d3df7e16dae539e01
Steven Riley <rileystevend@gmail.com>
2018-03-26 20:49:28 -0500

    all files linted ex-epk edit, login and homepage components

--- client/src/components/requests.jsx
+++ client/src/components/requests.jsx
@@ -1,57 +1,49 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import * as actions from '../actions/index.js';
 import { bindActionCreators } from 'redux';
-import ReactDOM from 'react-dom';
-import $ from 'jquery';
+import { Tabs, List } from 'antd';
 import axios from 'axios';
-import calendar from './calendar.jsx';
-import { Modal, Tabs, List, Button, Layout, Menu, Breadcrumb, Icon, Spin } from 'antd';
-const SubMenu = Menu.SubMenu;
-const TabPane = Tabs.TabPane;
+import * as actions from '../actions/index.js';
+
 const moment = require('moment');
 
+const { TabPane } = Tabs;
+//* ******************************************DO NOT LINT, WORK IN PROGRESS*******************
+
 class Requests extends React.Component {
-
   constructor(props) {
-    super(props)
-    let bookings = this.props.store.bookings;
+    super(props);
+    const { bookings } = this.props.store;
     this.state = {
-      pending: bookings.filter((booking)=> booking.confirmed === 0),
-      confirmed: bookings.filter((booking)=> booking.confirmed === 1),
-      loadingMore: false,
-      showLoadingMore: true,
-    }
-  }
-
-  componentDidMount() {
+      pending: bookings.filter(booking => booking.confirmed === 0),
+      confirmed: bookings.filter(booking => booking.confirmed === 1),
+      // loadingMore: false,
+      // showLoadingMore: true,
+    };
   }
 
   onSeeEventClick(item) {
-    console.log(item)
   }
 
-  onSeeVenueDetailsClick (item) {
-    console.log(item)
+  onSeeVenueDetailsClick(item) {
   }
 
 
-//ADD ABILITY TO UNCONFIRM EVENT
+  // ADD ABILITY TO UNCONFIRM EVENT
   onConfirmClick(item) {
-    console.log(item)
+    console.log(item);
     axios.patch('/booking', item)
-      .then(res => {
-        let updatedBookings = res.data.bookings;
-        this.props.actions.setBookings(updatedBookings)
+      .then((res) => {
+        const updatedBookings = res.data.bookings;
+        this.props.actions.setBookings(updatedBookings);
         this.setState({
-          pending: updatedBookings.filter((booking)=> booking.confirmed === 0),
-          confirmed: updatedBookings.filter((booking)=> booking.confirmed === 1),
-        })
-      }).catch(err => console.log(err))
+          pending: updatedBookings.filter(booking => booking.confirmed === 0),
+          confirmed: updatedBookings.filter(booking => booking.confirmed === 1),
+        });
+      }).catch(err => console.log(err));
   }
 
   onEpkClick(item) {
-    console.log(item)
   }
 
 
@@ -63,83 +55,83 @@ class Requests extends React.Component {
 
 
   render() {
-  	const { confirmed, pending } = this.state;
+    const { confirmed, pending } = this.state;
     const isArtist = this.props.store.artist;
-  	// const loadMore = showLoadingMore ? (
-   //  <div style={{ textAlign: 'center', marginTop: 12, height: 32, lineHeight: '32px' }}>
-   //    {loadingMore && <Spin />}
-   //    {!loadingMore && <Button onClick={}>loading more</Button>}
-   //  </div>
-  // ) : null;
-      return (
-        <div>
-          <Tabs defaultActiveKey="1" onChange={this.callback}>
-            <TabPane tab="Confirmed" key="1">
-              <List
-      	        className="demo-loadmore-list"
-      	        itemLayout="horizontal"
-      	        dataSource={confirmed}
-      	        renderItem={item => {
-                  let name;
-                  let time = item.start_time || '';
-                  if (isArtist === true) {
-                    name = item.venue_name;
-                  } else {
-                    name = item.artist_name;
-                  }
-                  return (
-        	          <List.Item actions={[<a>See Event Details</a>]}>
-        	            <List.Item.Meta
-        	              title={<a href="https://ant.design">{name}</a>}
-        	              description={item.booking_description}
-        	            />
-                      <div>Gig on: {moment(time.slice(0, 10)).format("MMM Do YY")}</div>
-        	          </List.Item>
-                  )
+    // const loadMore = showLoadingMore ? (
+    //  <div style={{ textAlign: 'center', marginTop: 12, height: 32, lineHeight: '32px' }}>
+    //    {loadingMore && <Spin />}
+    //    {!loadingMore && <Button onClick={}>loading more</Button>}
+    //  </div>
+    // ) : null;
+    return (
+      <div>
+        <Tabs defaultActiveKey="1" onChange={this.callback}>
+          <TabPane tab="Confirmed" key="1">
+            <List
+              className="demo-loadmore-list"
+              itemLayout="horizontal"
+              dataSource={confirmed}
+              renderItem={(item) => {
+                let name;
+                const time = item.start_time || '';
+                if (isArtist === true) {
+                  name = item.venue_name;
+                } else {
+                  name = item.artist_name;
+                }
+                return (
+                  <List.Item actions={[<a>See Event Details</a>]}>
+                    <List.Item.Meta
+                      title={<a href="https://ant.design">{name}</a>}
+                      description={item.booking_description}
+                    />
+                    <div>Gig on: {moment(time.slice(0, 10)).format('MMM Do YY')}</div>
+                  </List.Item>
+                  );
               }}
-              />
-            </TabPane>
-            <TabPane tab="Pending" key="2">
-              <List
-                className="demo-loadmore-list"
-                itemLayout="horizontal"
-                dataSource={pending}
-                renderItem={item => {
-                  let name;
-                  let time = item.start_time || '';
-                  let subtab = [];
-                  if (isArtist === true) {
-                    name = item.venue_name;
-                    subtab.push(<a onClick={() => this.onSeeVenueDetailsClick(item)}>See Venue Details</a>);
-                  } else {
-                    name = item.artist_name;
-                    subtab.push(<a onClick={() => this.onConfirmClick(item)}>Confirm Event</a>, <a>See EPK</a>);
-                  }
-                  return (
-                    <List.Item actions={subtab}>
-                      <List.Item.Meta
-                        title={<a href="https://ant.design">{name}</a>}
-                        description={item.booking_description}
-                      />
-                      <div>Trying to gig: {moment(time.slice(0, 10)).format("MMM Do YY")}</div>
-                    </List.Item>
-                  )
-                }}
-              />
-            </TabPane>
-          </Tabs>
-        </div>
-      );
+            />
+          </TabPane>
+          <TabPane tab="Pending" key="2">
+            <List
+              className="demo-loadmore-list"
+              itemLayout="horizontal"
+              dataSource={pending}
+              renderItem={(item) => {
+                let name;
+                const time = item.start_time || '';
+                const subtab = [];
+                if (isArtist === true) {
+                  name = item.venue_name;
+                  subtab.push(<a onClick={() => this.onSeeVenueDetailsClick(item)}>See Venue Details</a>);
+                } else {
+                  name = item.artist_name;
+                  subtab.push(<a onClick={() => this.onConfirmClick(item)}>Confirm Event</a>, <a>See EPK</a>);
+                }
+                return (
+                  <List.Item actions={subtab}>
+                    <List.Item.Meta
+                      title={<a href="https://ant.design">{name}</a>}
+                      description={item.booking_description}
+                    />
+                    <div>Trying to gig: {moment(time.slice(0, 10)).format('MMM Do YY')}</div>
+                  </List.Item>
+                );
+              }}
+            />
+          </TabPane>
+        </Tabs>
+      </div>
+    );
   }
 }
 
 const mapStateToProps = state => (
     { store: state } // eslint-disable-line
-  );
+);
 
-  const mapDispatchToProps = dispatch => (
-    { actions: bindActionCreators(actions, dispatch) }
-  );
+const mapDispatchToProps = dispatch => (
+  { actions: bindActionCreators(actions, dispatch) }
+);
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(Requests);
--- client/src/components/searchVenues.jsx
+++ client/src/components/searchVenues.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import * as actions from '../actions/index.js';
 import { bindActionCreators } from 'redux';
-import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import { Input, Table, Divider, Modal } from 'antd';
-const Search = Input.Search;
 import axios from 'axios';
+import { Input, Table, Divider, Modal } from 'antd';
+import * as actions from '../actions/index.js';
 import calendar from './calendar.jsx';
 
+const { Search } = Input;
 
 class SearchVenues extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       saveToStore: this.props.actions.addBooking.bind(this),
       venues: [],
@@ -21,7 +18,6 @@ class SearchVenues extends React.Component {
       loading: false,
       fetched: false,
       venueBookings: [],
-      venueName: '',
       columns: [{
         title: 'Name',
         key: 'name',
@@ -43,91 +39,95 @@ class SearchVenues extends React.Component {
         key: 'calendar',
         render: (text, record) => (
           <span>
-            <a href="#" onClick={() => {
-              this.viewCalendar(record.venue_id, record.venue_name)
-            }}>View Venue Calendar</a>
+            <a
+              href="#"
+              onClick={() => {
+                this.viewCalendar(record.venue_id, record.venue_name);
+              }}
+            >
+            View Venue Calendar
+            </a>
           </span>
-        )
-      }]
-    }
+        ),
+      }],
+    };
   }
 
   onSearch(info) {
     axios.get('/venues', {
       params: {
-        city: info
-      }
-    }).then(res => {
+        city: info,
+      },
+    }).then((res) => {
       this.setState({
         fetched: true,
-        venues: res.data.venues
-      })
-    }).catch(err => {
-      console.error('error', err)
-    })
+        venues: res.data.venues,
+      });
+    }).catch((err) => {
+      console.error('error', err);
+    });
   }
 
-  viewCalendar (id, venueName) {
-  let artistId = this.props.store.artistId;
-  axios.get('/venueCalendar', {
-    params: {
-      venue_id: id
-    }
-  }).then((res) => {
-    this.setState({
-      venueBookings: res.data,
-    })
-    Modal.success({
-      title: 'Venue Calendar',
-      content: calendar(this.state.venueBookings, true, artistId, id, this.state.saveToStore, venueName)
-    })
-  }).catch((err) => {
-    console.error('error', err);
-  })
-};
+  viewCalendar(id, venueName) {
+    const { artistId } = this.props.store;
+    axios.get('/venueCalendar', {
+      params: {
+        venue_id: id,
+      },
+    }).then((res) => {
+      this.setState({
+        venueBookings: res.data,
+      });
+      Modal.success({
+        title: 'Venue Calendar',
+        content: calendar(this.state.venueBookings, true, artistId, id, this.state.saveToStore, venueName),
+      });
+    }).catch((err) => {
+      console.error('error', err);
+    });
+  }
 
   render() {
     if (this.state.fetched === false) {
       return (
         <Search
-        placeholder="Enter A City Here"
-        onSearch={value => this.onSearch(value)}
-        style={{ width: 300 }}
-        enterButton
+          placeholder="Enter A City Here"
+          onSearch={value => this.onSearch(value)}
+          style={{ width: 300 }}
+          enterButton
         />
       );
-    } else {
-      return (
-        <div>
+    }
+    return (
+      <div>
         <Search
-        placeholder="Enter A City Here"
-        onSearch={value => this.onSearch(value)}
-        style={{ width: 300 }}
-        enterButton
+          placeholder="Enter A City Here"
+          onSearch={value => this.onSearch(value)}
+          style={{ width: 300 }}
+          enterButton
         />
         <Divider />
         <Table
-        columns={this.state.columns}
-        rowKey={(record) => record.registered}
-        onClick={this.viewCalendar}
-        dataSource={this.state.venues}
-        pagination={this.state.pagination}
-        loading={this.state.loading}
-        onChange={this.handleTableChange}
-      />
+          columns={this.state.columns}
+          rowKey={record => record.registered}
+          onClick={this.viewCalendar}
+          dataSource={this.state.venues}
+          pagination={this.state.pagination}
+          loading={this.state.loading}
+          onChange={this.handleTableChange}
+        />
       </div>
-      );
-    }
+    );
   }
 }
 
 const mapStateToProps = state => (
     { store: state } // eslint-disable-line
-  );
+);
 
-  const mapDispatchToProps = dispatch => (
-    { actions: bindActionCreators(actions, dispatch) }
-  );
+const mapDispatchToProps = dispatch => (
+  { actions: bindActionCreators(actions, dispatch) }
+);
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchVenues);
