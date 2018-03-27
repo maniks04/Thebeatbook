81208f3ab8111b2c88417ca1541a1991972a5ffd
Manik Sewak <Maniks04@aol.com>
2018-03-27 16:28:10 -0500

    test

--- server/index.js
+++ server/index.js
@@ -146,13 +146,9 @@ app.get('/epk', async (req, res) => {
   res.status(200).send({ epk });
 });
 
-app.all('/*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '/../client/dist/index.html'));
 });
-// app.all('/*', function(req, res, next) {
-//   // Just send the index.html for other files to support HTML5Mode
-//   res.sendFile('index.html', { root: __dirname });
-// });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('listening on port 3000!');
