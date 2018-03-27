efbaa0ba2b44b566cc8055b68eb06bb32112fda2
Trent <trenthendricks2@gmail.com>
2018-03-26 19:47:35 -0500

    cleaned epkedit

--- client/src/components/epkEdit.jsx
+++ client/src/components/epkEdit.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import * as actions from '../actions/index.js';
-import {open} from '../actions'
-import { bindActionCreators } from 'redux';
-import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import axios from 'axios';
-import { Form, Icon, Input, Button, Upload } from 'antd';
+import { Form, Icon, Input, Button, Upload } from 'antd'; /* eslint-disable-line */
 const FormItem = Form.Item;
 const { TextArea } = Input;
 
@@ -17,24 +12,30 @@ function getBase64(img, callback) {
   reader.readAsDataURL(img);
 }
 
-function beforeUpload(file) {
-  const isJPG = file.type === 'image/jpeg';
-  if (!isJPG) {
-    message.error('You can only upload JPG file!');
-  }
-  const isLt2M = file.size / 1024 / 1024 < 2;
-  if (!isLt2M) {
-    message.error('Image must smaller than 2MB!');
-  }
-  return isJPG && isLt2M;
-}
-
 class NormalLoginForm extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       loading: false,
-    }
+    };
+  }
+
+  onClick() {
+    const artist_id = this.props.artistID;
+    const artist_name = $('.band').val();
+    const artist_description = $('.description').val();
+    const artist_city = $('.city').val();
+    const artist_state = $('.state').val();
+    axios.post('/updateEPK', {
+      artist_name,
+      artist_description,
+      artist_city,
+      artist_state,
+      artist_id,
+    }).then(() => {
+    }).catch((err) => {
+      console.error(err) /* eslint-disable-line */
+    });
   }
 
   handleChange(info) {
@@ -51,24 +52,6 @@ class NormalLoginForm extends React.Component {
     }
   }
 
-  onClick() {
-    let artist_id = this.props.artistID
-    let artist_name = $('.band').val();
-    let artist_description = $('.description').val();
-    let artist_city = $('.city').val();
-    let artist_state = $('.state').val();
-    axios.post('/updateEPK', {
-      artist_name: artist_name,
-      artist_description: artist_description,
-      artist_city: artist_city,
-      artist_state: artist_state,
-      artist_id: artist_id
-    }).then(res => {
-    }).catch(err => {
-      console.error(err)
-    })
-  }
-
   render() {
     const uploadButton = (
       <div>
@@ -76,34 +59,34 @@ class NormalLoginForm extends React.Component {
         <div className="ant-upload-text">Upload</div>
       </div>
     );
-    const imageUrl = this.state.imageUrl;
+    const { imageUrl } = this.state;
 
     return (
       <Form className="login-form">
         <FormItem>
-            <Input
-              className='band'
-              placeholder='Band Name?'
-            ></Input>
+          <Input
+            className="band"
+            placeholder="Band Name?"
+          />
         </FormItem>
         <FormItem>
           <Input
-            className='city'
-            placeholder='Your City?'
-          ></Input>
+            className="city"
+            placeholder="Your City?"
+          />
         </FormItem>
         <FormItem>
           <Input
-            className='state'
-            placeholder='Your State?'
-          ></Input>
+            className="state"
+            placeholder="Your State?"
+          />
         </FormItem>
         <FormItem>
-            <TextArea
-              className='description'
-              placeholder="Please Insert Your Band Bio Here!"
-              autosize={{ minRows: 4, maxRows: 12 }}
-             />
+          <TextArea
+            className="description"
+            placeholder="Please Insert Your Band Bio Here!"
+            autosize={{ minRows: 4, maxRows: 12 }}
+          />
         </FormItem>
         <Upload
           name="avatar"
@@ -111,8 +94,8 @@ class NormalLoginForm extends React.Component {
           className="avatar-uploader"
           showUploadList={false}
           action="//jsonplaceholder.typicode.com/posts/"
-          beforeUpload={beforeUpload}z
-          onChange={this.handleChange} >
+          onChange={this.handleChange}
+        >
           {imageUrl ? <img src={imageUrl} alt="" /> : uploadButton}
         </Upload>
         <FormItem>
@@ -120,7 +103,8 @@ class NormalLoginForm extends React.Component {
             type="primary"
             htmlType="submit"
             onClick={() => this.onClick()}
-            className="login-form-button">
+            className="login-form-button"
+          >
             Save Changes
           </Button>
         </FormItem>
