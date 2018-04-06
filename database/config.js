da0a6d67044250157bd130b224c0dd80585becc1
Steven Riley <rileystevend@gmail.com>
2018-04-06 09:18:19 -0500

    Apr 6 2018

--- database/config.js
+++ database/config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DB,
-  port: process.env.DB_PORT,
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'beatbook',
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_DB,
+  // port: process.env.DB_PORT,
 };
 
 // export DB_HOST=thebeatbook.cf0tcwamdz9t.us-west-2.rds.amazonaws.com
